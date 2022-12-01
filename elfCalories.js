7fdc174460dd1ddf39a754777fdedb20d2bb78ab
Henry Sabio <henry@sabio.sh>
2022-12-01 16:45:48 -0500

    Remove carriage returns

--- elfCalories.js
+++ elfCalories.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 // Grab Elf Calorie data from file and split into array seperated by empty lines
 const elves = fs.readFileSync('puzzleInput.txt', 'utf8')
-    .toString() // Remove carriage returns
+    .replace(/\r/g, '') // Remove carriage returns
     .trim() // Remove trailing newline
     .split('\n\n'); // Split into array by empty lines
 
@@ -23,3 +23,4 @@ console.log(Math.max(...summedCalories));
 // Find the top 3 highest calorie values and sum them
 let topThree = summedCalories.sort((a, b) => b - a).slice(0, 3);
 console.log(topThree.reduce((a, b) => a + b));
+console.log(topThree);
