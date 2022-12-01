617d7e60dd2a0781ffe5e703dd0b1cf9ebfa4b10
Henry Sabio <henry@sabio.sh>
2022-12-01 16:33:44 -0500

    Sum up calories in bag of each elf

--- elfCalories.js
+++ elfCalories.js
@@ -1,9 +1,18 @@
 const fs = require('fs');
 // Grab Elf Calorie data from file and split into array seperated by empty lines
 const elves = fs.readFileSync('puzzleInput.txt', 'utf8')
-    .replace(/\r/g, '') // Remove carriage returns
+    .toString() // Remove carriage returns
     .trim() // Remove trailing newline
     .split('\n\n'); // Split into array by empty lines
 
-console.log(elves);
+// Create array of arrays of calories
+let calories = elves.map(elf => elf.split('\n'));
 
+// Sum up calories for each elf
+let summedCalories = calories.map(elf => {
+    let total = 0;
+    elf.forEach(cal => { // For each cal
+        total += parseInt(cal); // Add to total
+    });
+    return total;
+});
